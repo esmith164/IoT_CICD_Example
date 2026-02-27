1b87adf35007d1867b53312cfa825f453dce3c29
tylerrleee <160276402+tylerrleee@users.noreply.github.com>
2026-02-27 15:48:29 -0500

    fail test

--- src/test/temperature.test.ts
+++ src/test/temperature.test.ts
@@ -9,4 +9,7 @@ describe("isOverheating", () => {
   it("returns false if temp is 70 or below", () => {
     expect(isOverheating(70)).toBe(false);
   });
+  it("returns true if temp is 67", () => {
+    expect(isOverheating(67)).toBe(true);
+  });
 });
